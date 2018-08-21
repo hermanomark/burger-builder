0af6924a9dc684e72b3b36db67046a5a8e2b4a0c
Mark Hermano <hermano.mark.mh@gmail.com>
2018-08-21 22:55:28 +0800

    sending post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -84,7 +85,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHanlder = () => {
-    alert('You continue');
+    // alert('You continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPric,
+      customer: {
+        name: 'Mark Hermano',
+        address: {
+          street: 'Jensen St.',
+          zipCode: '1900',
+          country: 'Philippines'
+        },
+        email: 'hermano.mark.mh@gmail.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render() {
